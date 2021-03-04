b985e62a36cf0c3ba1639461344eb828baad119a
dopo1798 <dipikamonas1798@gmail.com>
2021-03-05 01:05:27 +0530

    second day commit

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require('express');
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json());
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.get('/start', function(req, res) {
+    const responseObj = {
+        fn: "dipika",
+        ln: "sarkar"
+    }
+    res.send(responseObj);
+});
+app.post('/start', function(req, res) {
+    // const responseObj = {
+    //     fn: "dipika",
+    //     ln: "sarkar"
+    // }
+    console.log(req.body);
+    res.send("this is post method");
+});
 
 app.listen(3000, 'localhost', function run() {
     console.log('App is running 3000 port');
