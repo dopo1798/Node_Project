84034480e7a6056f3faeebbb623ca77b74b7b021
dopo1798 <dipikamonas1798@gmail.com>
2021-03-06 00:52:02 +0530

    6tm march

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var express = require('express');
 // app.use(bodyParser.json());
 var app = express();
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 app.use(bodyParser.json());
+const DB_USER = 'dipika_user';
+const PASSWORD = encodeURIComponent('dopo1234');
+const DB_URL = `mongodb://${DB_USER}:${PASSWORD}@localhost:27017/nwtonSchool`;
+mongoose.connect(DB_URL).then(() => {
+    console.log("Database connected successfully");
+}).catch((err) => {
+    console.log(err);
+});
 app.get('/start', function(req, res) {
     const responseObj = {
         fn: "dipika",
